c2fdbd70fb07e92455a8a7dfec0b71ad25d7da61
Harrison-Holt <hholt2901@gmail.com>
2025-01-02 23:33:51 -0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Home from './frontend/home.jsx'; 
+
+function App() {
+
+  return (
+    <>
+    <Home></Home>
+    </>
+  )
+}
+
+export default App
---
+++ src/frontend/home.jsx
@@ -0,0 +1,14 @@
+import { AppBar, Container, Box, Button, Typograph, TextField } from 'mui'; 
+
+function Home() {
+
+    return (
+        <AppBar>
+            <Typograph variant='h1'>
+                Rolling Pines Lodge
+            </Typograph>
+        </AppBar>
+    ); 
+}
+
+export default Home; 
