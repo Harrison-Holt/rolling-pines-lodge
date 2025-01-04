224bfcc05424b281cbf1ccb0a28771c49dc9b437
Harrison-Holt <hholt2901@gmail.com>
2025-01-03 23:11:56 -0500

    Initial Commit

--- src/frontend/home.jsx
+++ src/frontend/home.jsx
@@ -38,8 +38,6 @@ function Homepage() {
 
       {/* Main Content */}
       <Box className="main-content"
-      component="img"
-      src={hero_section_image}
       >
         <Typography variant="h4">Welcome to Rolling Pines Lodge</Typography>
         <Typography variant="body1">Experience fine dining amidst nature's beauty.</Typography>
