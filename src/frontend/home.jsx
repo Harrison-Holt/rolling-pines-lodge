18c2b15faeec3959af4b70c48fe5f90684a5def2
Harrison-Holt <hholt2901@gmail.com>
2025-01-03 19:52:21 -0500

    Initial Commit

--- src/frontend/home.jsx
+++ src/frontend/home.jsx
@@ -1,43 +1,74 @@
-import { AppBar, Container, Box, Button, Typography, TextField } from '@mui/material'; 
-import logo from '../assets/Rolling_Pines_Lodge_Logo.png'; 
+import React from 'react';
+import { AppBar, Container, Box, Typography, Toolbar, Button, IconButton } from '@mui/material';
+import logo from '../assets/Rolling_Pines_Lodge_Logo.png';
 import { Home, MenuBook } from '@mui/icons-material';
 import TableRestaurantIcon from '@mui/icons-material/TableRestaurant';
 
 function Homepage() {
+  return (
+    <Container disableGutters maxWidth="xl" className='body'>
+      {/* Top Navigation Bar */}
+      <AppBar position="static" sx={{ backgroundColor: '#451a61' }}>
+        <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+          
+          {/* Logo Section */}
+          <Box
+            sx={{
+              height: '75px',
+              width: '75px',
+              backgroundImage: `url(${logo})`,
+              backgroundSize: 'contain',
+              backgroundRepeat: 'no-repeat',
+              backgroundPosition: 'center',
+            }}
+          />
 
-    return (
-        <Container className='body'>
-        <AppBar className='header'>
-            <Box sx={{ 
-                height: '75px', 
-                backgroundImage: `url(${logo})`, 
-                backgroundSize: 'cover', 
-                backgroundPosition: 'center', 
-                width: '10%'
-            }}>
-            </Box>
-            <Box sx={{
-                display: 'flex', 
-                flexDirection: 'row', 
-                justifyContent: 'space-around',
-                alignContent: 'center'
-            }}>
-                <Card>
-                <Typography component='a' href='#'>Home</Typography>
-                <Home/>
-                </Card>
-                <Card>
-                <Typography component='a' href='#'>Menu</Typography>
-                <MenuBook/>
-                </Card>
-                <Card>
-                <Typography component='a' href='#'>Reservations</Typography>
-                <TableRestaurantIcon/>
-                </Card>
-            </Box>
-        </AppBar>
-        </Container>
-    ); 
+          {/* Navigation Links */}
+          <Box sx={{
+            display: 'flex',
+            gap: 3,
+            alignItems: 'center'
+          }}>
+            <Button
+              startIcon={<Home />}
+              href="#"
+              sx={{ color: '#f5f5f5', textTransform: 'none', fontSize: '1rem' }}
+            >
+              Home
+            </Button>
+
+            <Button
+              startIcon={<MenuBook />}
+              href="#"
+              sx={{ color: '#f5f5f5', textTransform: 'none', fontSize: '1rem' }}
+            >
+              Menu
+            </Button>
+
+            <Button
+              startIcon={<TableRestaurantIcon />}
+              href="#"
+              sx={{ color: '#f5f5f5', textTransform: 'none', fontSize: '1rem' }}
+            >
+              Reservations
+            </Button>
+          </Box>
+
+          {/* Call-to-Action Button */}
+          <Button
+            variant="contained"
+            color="secondary"
+            sx={{
+              textTransform: 'none',
+              fontWeight: 'bold'
+            }}
+          >
+            Book Now
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Container>
+  );
 }
 
-export default Homepage; 
+export default Homepage;
