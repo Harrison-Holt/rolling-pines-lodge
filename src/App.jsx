8543494cdf2ebdd77fceff3e394ec748b0538ec3
Harrison-Holt <hholt2901@gmail.com>
2025-01-05 00:30:12 -0500

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useRoutes } from 'react-router-dom';
 import Homepage from './frontend/home.jsx';
-import Menu from './Menu';
+import Menu from './frontend/menu.jsx';
 import Cart from './frontend/cart.jsx'; 
 
 function App() {
