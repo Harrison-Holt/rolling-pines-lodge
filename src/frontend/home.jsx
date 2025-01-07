85bb7f96b231552b96341666680172ff19a6b887
Harrison Holt <116909429+Harrison-Holt@users.noreply.github.com>
2025-01-06 20:46:04 -0500

    Update home.jsx

--- src/frontend/home.jsx
+++ src/frontend/home.jsx
@@ -37,14 +37,14 @@ const featuredDishes = [
   function Homepage() {
     return (
       <>
-        <AppBar position="fixed" sx={{ backgroundColor: '#451a61', paddingLeft: 0, paddingRight: 0 }}>
-          <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', paddingLeft: 0, paddingRight: 0 }}>
+        <AppBar position="fixed" sx={{ backgroundColor: '#451a61'}}>
+          <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
             {/* Left Section: Logo */}
             <Box
               component="img"
               src={logo}
               alt="Rolling Pines Lodge Logo"
-              sx={{ height: 100, width: 100, marginLeft: 0 }}
+              sx={{ height: 100, width: 100, }}
             />
   
             {/* Right Section: Navigation Links and Button */}
